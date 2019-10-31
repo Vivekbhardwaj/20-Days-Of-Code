17e4a260bfb90ff5c1cde892e89dda7459528fbd
vivek <vivekbhardwaj0067@gmail.com>
2019-11-01 01:59:32 +0530

    Eulers tour of a rooted tree

---
+++ Graphs/Tree Basics/Euler_tour_of_tree.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void eulers_tour(vector<vector<int>>&tree,vector<int>&euler,int current_node){
+    euler.push_back(current_node);
+    for(auto nodes : tree[current_node]){
+        eulers_tour(tree,euler,nodes);
+
+        euler.push_back(current_node);
+    }
+        
+}
+
+int main(){
+    vector<vector<int>>v(8);
+    v[0].push_back(1);
+    v[0].push_back(2);
+    v[0].push_back(3);
+    v[1].push_back(4);
+    v[1].push_back(5);
+    v[2].push_back(6);
+    v[6].push_back(7);
+    vector<int> euler;
+    eulers_tour(v,euler,0);
+    for(auto i : euler) cout<<i<<" ";
+}
